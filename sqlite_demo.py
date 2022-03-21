fe9b1372634650799190933ef06e5456f8661568
Shubham Sharma <shubhamsharma960808@gmail.com>
2022-03-21 22:19:02 +0530

    second commit

--- sqlite_demo.py
+++ sqlite_demo.py
@@ -4,10 +4,13 @@ conn = sqlite3.connect("test.db")
 c = conn.cursor()
 
 # c.execute('''INSERT INTO USERS (username, password, email) 
-# VALUES ("Henry", "mypassword", "abc")''')
+# VALUES ("Hero", "mypassword", "abc")''')
 
-c.execute('''SELECT * FROM USERS
-WHERE username = "sharma"''')
+c.execute('''SELECT * FROM USERS''')
+
+rows = c.fetchall()
+print(rows)
+
 conn.commit()
 
 conn.close()
